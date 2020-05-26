ef33d283be4775a66a10cbea32a159d836812d93
smelc <smelc@users.noreply.github.com>
2020-05-26 15:05:51 +0200

    Move @polux fix of flickering to Event.hs, to weightwatcher View.hs

--- app/Event.hs
+++ app/Event.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Event where
+module Event
+  ( onEvent,
+    onMouseEnter',
+    onMouseLeave',
+  )
+where
 
 import Data.Aeson
 import Miso
@@ -19,3 +24,24 @@ onEvent ::
   Attribute Action
 onEvent s =
   on s clientXDecoder $ uncurry DragXY
+
+-- | Extracts the class name of the target from an event.
+classNameDecoder :: Decoder Value
+classNameDecoder =
+  Decoder
+    { decodeAt = DecodeTarget ["target"],
+      decoder = withObject "target" $ \o -> o .: "className"
+    }
+
+-- | Helper function for onMouseEnter' and onMouseLeave'.
+onMouseEvent eventName className action =
+  on eventName classNameDecoder $ \(String name) ->
+    if name == className then action else NoOp
+
+-- | Like onMouseEnter, but restricts event firing to elements with the
+-- provided class name.
+onMouseEnter' = onMouseEvent "mouseenter"
+
+-- | Like onMouseLeave, but restricts event firing to elements with the
+-- provided class name.
+onMouseLeave' = onMouseEvent "mouseleave"
--- app/View.hs
+++ app/View.hs
@@ -19,26 +19,6 @@ import Miso.String
 import Model
 import Update
 
--- | Extracts the class name of the target from an event.
-classNameDecoder :: Decoder Value
-classNameDecoder = Decoder {
-  decodeAt = DecodeTarget ["target"],
-  decoder = withObject "target" $ \o -> o .: "className"
-}
-
--- | Helper function for onMouseEnter' and onMouseLeave'.
-onMouseEvent eventName className action =
-  on eventName classNameDecoder $ \(String name) ->
-    if name == className then action else NoOp
-
--- | Like onMouseEnter, but restricts event firing to elements with the
--- provided class name.
-onMouseEnter' = onMouseEvent "mouseenter"
-
--- | Like onMouseLeave, but restricts event firing to elements with the
--- provided class name.
-onMouseLeave' = onMouseEvent "mouseleave"
-
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
 viewModel model@Model {board, uiCards} =
