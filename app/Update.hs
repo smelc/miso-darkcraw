7671425a20c3f0b6788a0ef13a51187589a11858
smelc <smelc@users.noreply.github.com>
2020-06-05 11:55:05 +0200

    First step towards changing model upon playing a card

--- app/Update.hs
+++ app/Update.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -13,21 +14,6 @@ import Miso
 import Miso.String
 import Model
 import Text.PrettyPrint.ANSI.Leijen
-
--- | Sum type for application events
-data Action
-  = -- | Dragging card in hand
-    DragStart HandIndex
-  | DragEnd
-  | DragEnter CardSpot
-  | DragLeave CardSpot
-  | -- | Starting hovering card in hand
-    InHandMouseEnter HandIndex
-  | -- | Ending hovering card in hand
-    InHandMouseLeave HandIndex
-  | NoOp
-  | SayHelloWorld
-  deriving (Show, Eq)
 
 instance ToExpr CreatureKind
 
@@ -67,6 +53,22 @@ instance ToExpr Interaction
 
 instance ToExpr Model
 
+-- | Sum type for application events. If drop stuff doesn't work
+-- | think whether it's affected by https://github.com/dmjio/miso/issues/478
+data Action
+  = -- | Dragging card in hand
+    DragStart HandIndex
+  | DragEnd
+  | DragEnter CardSpot
+  | DragLeave CardSpot
+  | -- | Starting hovering card in hand
+    InHandMouseEnter HandIndex
+  | -- | Ending hovering card in hand
+    InHandMouseLeave HandIndex
+  | NoOp
+  | SayHelloWorld
+  deriving (Show, Eq)
+
 logUpdates :: (Monad m, Eq a, ToExpr a) => (Action -> a -> m a) -> Action -> a -> m a
 logUpdates update NoOp model = update NoOp model
 logUpdates update action model = do
@@ -81,21 +83,35 @@ logUpdates update action model = do
       | otherwise = prettyDiff (ediff model model')
     prettyDiff edits = displayS (renderPretty 0.4 80 (ansiWlEditExprCompact edits)) ""
 
-updateI :: Action -> Interaction -> Interaction
+data PlayAction
+  = -- | Player puts a card from his on the board
+    Place HandIndex PlayerSpot CardSpot
+  | NoPlayAction
+
+updateI :: Action -> Interaction -> (Interaction, PlayAction)
 updateI (DragStart i) _ =
-  DragInteraction $ Dragging i Nothing
-updateI DragEnd _ = NoInteraction -- TODO: drop if on drop target
+  (DragInteraction $ Dragging i Nothing, NoPlayAction)
+updateI DragEnd _ = (NoInteraction, NoPlayAction) -- TODO: drop if on drop target
 -- DragEnter cannot create a DragInteraction if there's none yet, we don't
 -- want to keep track of drag targets if a drag action did not start yet
 updateI (DragEnter cSpot) (DragInteraction dragging) =
-  DragInteraction $ dragging {dragTarget = Just cSpot}
+  (DragInteraction $ dragging {dragTarget = Just cSpot}, NoPlayAction)
 updateI (DragLeave _) (DragInteraction dragging) =
-  DragInteraction $ dragging {dragTarget = Nothing}
-updateI (InHandMouseEnter i) NoInteraction = HoverInteraction $ Hovering i
-updateI (InHandMouseLeave _) _ = NoInteraction
-updateI _ i = i
+  (DragInteraction $ dragging {dragTarget = Nothing}, NoPlayAction)
+updateI (InHandMouseEnter i) NoInteraction = (HoverInteraction $ Hovering i, NoPlayAction)
+updateI (InHandMouseLeave _) _ = (NoInteraction, NoPlayAction)
+updateI _ i = (i, NoPlayAction)
+
+play :: Model -> PlayAction -> Model
+play m@Model{board} =
+  \case
+    Place (HandIndex i) pSpot cSpot-> m -- FIXME smelc forward to board
+    NoPlayAction -> m
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
 updateModel SayHelloWorld m = m <# do consoleLog "miso-darkcraw says hello" >> pure NoOp
-updateModel a m@Model {interaction} = noEff $ m {interaction = updateI a interaction}
+updateModel a m@Model {interaction} =
+  noEff $ (play m playAction) {interaction = interaction'}
+  where
+    (interaction', playAction) = updateI a interaction
