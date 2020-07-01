ec3c0c960a58ae99a35f4b53e015bb8ac65c71d5
Paul Brauner <polux@google.com>
2020-07-01 14:15:40 +0200

    Add an hie.yaml to fix IDE. Fix errors that have now suddenly appeared.

--- app/shared/Event.hs
+++ app/shared/Event.hs
@@ -10,6 +10,7 @@ import Data.Aeson
 import Miso
 import Miso.String
 import Update
+import Data.Text (Text)
 
 -- | Extracts the class name of the target from an event.
 classNameDecoder :: Decoder Value
@@ -20,14 +21,17 @@ classNameDecoder =
     }
 
 -- | Helper function for onMouseEnter' and onMouseLeave'.
+onMouseEvent :: MisoString -> Text -> Action -> Attribute Action
 onMouseEvent eventName className action =
   on eventName classNameDecoder $ \(String name) ->
     if name == className then action else NoOp
 
 -- | Like onMouseEnter, but restricts event firing to elements with the
 -- provided class name.
+onMouseEnter' :: Text -> Action -> Attribute Action
 onMouseEnter' = onMouseEvent "mouseenter"
 
 -- | Like onMouseLeave, but restricts event firing to elements with the
 -- provided class name.
+onMouseLeave' :: Text -> Action -> Attribute Action
 onMouseLeave' = onMouseEvent "mouseleave"
