bd81021b1c0b7f044d063a19b012868e243b593b
smelc <smelc@users.noreply.github.com>
2020-08-21 14:24:19 +0200

    Add script to publish game to https://hgames.itch.io/pixel-card-wars from the command line

---
+++ scripts/itch-push.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Script to publish the game to https://hgames.itch.io/pixel-card-wars
+
+if [[ ! $(command -v butler) ]]; then
+  echo "You need butler installed to execute this script: https://itch.io/docs/butler/"
+  exit 1
+fi
+
+rm -Rf "$TMP_PATH"  # Erase previous run if any
+
+# Compile
+export PCW_FLAVOR="itch"
+(cd app && rm -Rf .ghc.environment.* -Rf && nix-build) || { echo "compilation failed"; exit 1; }
+unset PCW_FLAVOR
+
+# Copy nix's result to /tmp
+declare -r ARCHIVE_NAME="pixel-card-wars.zip"
+declare -r TMP_PATH="/tmp/app.jsexe"
+declare -r ARCHIVE_PATH="${TMP_PATH}/${ARCHIVE_NAME}"
+cp -R "app/result-2/bin/app.jsexe" "$TMP_PATH"
+
+# Fix permissions, because everything is readonly in the nix store
+chmod +w "$TMP_PATH"
+find "$TMP_PATH" -name \* -exec chmod +w {} \;
+
+# Record version
+git rev-parse HEAD > "$TMP_PATH/VERSION" || { echo "Cannot record version"; exit 1; }
+
+# Create zip archive
+(cd "$TMP_PATH" && zip -r "${ARCHIVE_NAME}" .) || { echo "Cannot zip"; exit 1; }
+
+# Send to itch
+butler push "$ARCHIVE_PATH" hgames/pixel-card-wars:html || { echo "butler failed"; exit 1; }
+
+# Cleanup
+rm -Rf "$TMP_PATH"
