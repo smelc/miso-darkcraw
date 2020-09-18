00c62519ca40f933e1b63f8c07179d9b81a2432b
smelc <smelc@users.noreply.github.com>
2020-09-18 18:17:09 +0200

    Extend a bit the welcome scene. Run ./scripts/GenAssets.hs to update

--- app/shared/Movie.hs
+++ app/shared/Movie.hs
@@ -12,6 +12,12 @@ import Cinema
 w0 :: Element
 w0 = Actor 0 $ CreatureID General Human
 
+w01 :: Element
+w01 = Actor 2 $ CreatureID Spearman Human
+
+w02 :: Element
+w02 = Actor 2 $ CreatureID Archer Human
+
 w1 :: Element
 w1 = Actor 1 $ CreatureID Vampire Undead
 
@@ -21,12 +27,12 @@ welcomeMovie =
     (while 10) -- 10 tenth of seconds: 1 second
     [ w0 =: at' ToRight 0 15 <> w1 =: at 20 11,
       w0 =: right <> w1 =: left,
-      w0 =: right <> w1 =: left,
-      w1 =: tell "Fresh meat!",
-      w0 =: Leave
+      w0 =: right <> w0 =: tell "Come on guys!" <> w1 =: left,
+      w0 =: right <> w0 =: shutup <> w01 =: at' ToRight 0 15 <> w1 =: tell "Fresh meat!",
+      w0 =: right <> w01 =: right <> w02 =: at' ToRight 0 15,
+      w1 =: shutup,
+      w0 =: right <> w01 =: right <> w01 =: up <> w02 =: right,
+      w0 =: right <> w01 =: right <> w02 =: right,
+      w0 =: right <> w01 =: right <> w02 =: right,
+      w1 =: tell "Intriguing.."
     ]
-    ++ map
-      (while 10)
-      [ w1 =: shutup,
-        w1 =: tell "Intriguing.."
-      ]
