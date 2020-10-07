dab5f83b6ca193048efe0b8738cc602ad8fcf784
Paul Brauner <polux@google.com>
2020-10-07 12:46:52 +0200

    add missing file

---
+++ app/test/SceneEquivalence.hs
@@ -0,0 +1,23 @@
+module SceneEquivalence ((~=)) where
+
+import Cinema
+  ( ActorState,
+    Frame (Frame),
+    Scene,
+    TimedFrame (TimedFrame),
+    render,
+  )
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Test.Hspec (Expectation, shouldBe)
+
+data TimedRenderedFrame = TimedRenderedFrame Int (S.Set ActorState)
+  deriving (Eq, Show)
+
+forgetKeys :: TimedFrame ActorState -> TimedRenderedFrame
+forgetKeys (TimedFrame duration (Frame mapping)) = TimedRenderedFrame duration (S.fromList (M.elems mapping))
+
+(~=) :: Scene () -> Scene () -> Expectation
+scene1 ~= scene2 = framesOf scene1 `shouldBe` framesOf scene2
+  where
+    framesOf scene = map forgetKeys (render scene)
