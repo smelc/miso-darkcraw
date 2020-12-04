smelc/miso-darkcraw

commit 64eddd3cda46da21bacb913470151db942b981c2

Game: implement Infernal Haste. Next: support it in AI. 🤖

app/shared/Update.hs
@@ -29,7 +29,7 @@ import qualified Data.Text.Lazy as Text
 import Data.TreeDiff
 import qualified Data.Vector as V
 import Debug.Trace
-import qualified Game (Event (..), Result (..), Target (..), drawCards, nbCardsToDraw, nextAttackSpot, play, playAll, transferCards)
+import qualified Game (Event (..), PolyResult (..), Target (..), drawCards, nbCardsToDraw, nextAttackSpot, play, playAll, transferCards)
 import Miso
 import Miso.String (MisoString, fromMisoString)
 import Model
@@ -299,22 +299,25 @@ updateGameModel m (GameDragLeave _) (GameDragInteraction dragging)
 updateGameModel m@GameModel {board, gameShared} (GamePlay gameEvent) _ =
   case Game.play gameShared board gameEvent of
     Left errMsg -> withInteraction m $ GameShowErrorInteraction errMsg
-    Right (Game.Result board' anims') ->
+    Right (Game.Result board' nexts anims') ->
       -- There MUST be a delay here, otherwise it means we would need
       -- to execute this event now. We don't want that. 'playAll' checks that.
       (m', zip (repeat 1) $ maybeToList event)
       where
         m' = m {board = board', anims = anims'}
-        event = case gameEvent of
-          Game.Attack pSpot cSpot continue end ->
+        event = case (gameEvent, nexts) of
+          (Game.Attack pSpot cSpot continue end, Nothing) ->
             -- enqueue resolving next attack if applicable
             case (continue, Game.nextAttackSpot board pSpot (Just cSpot)) of
               (False, _) -> terminator
               (True, Nothing) -> terminator
               (True, Just cSpot') -> Just $ GamePlay $ Game.Attack pSpot cSpot' True end
             where
-              terminator = if end then Just GameIncrTurn else Nothing
-          _ -> Nothing
+              terminator = if end then Nothing else Just GameIncrTurn
+          (Game.Attack {}, Just e) ->
+            error $ "Cannot mix Game.Attack and events when enqueuing but got event: " ++ show e
+          (_, Nothing) -> Nothing
+          (_, _) -> event
 updateGameModel m@GameModel {board, gameShared, turn} (GameDrawCard n) _ =
   case Game.drawCards gameShared board pSpot 1 of
     Left errMsg -> withInteraction m $ GameShowErrorInteraction errMsg
@@ -355,7 +358,7 @@ updateGameModel m@GameModel {board, gameShared, turn} GameEndTurnPressed _ =
           -- Do not reveal player placement to AI
           let emptyPlayerInPlaceBoard = boardSetInPlace board pSpot Map.empty
           let placements = AI.placeCards gameShared emptyPlayerInPlaceBoard $ nextTurn turn
-          Game.Result board' boardui' <- Game.playAll gameShared board placements
+          Game.Result board' () boardui' <- Game.playAll gameShared board placements
           return $ m {anims = boardui', board = board'}
         else Right m
 updateGameModel m@GameModel {gameShared, playingPlayer, turn} GameIncrTurn _ =

app/shared/AI.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -20,13 +21,16 @@ import Data.List
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import qualified Data.Text as Text
-import Debug.Trace (trace)
+import Debug.Trace (trace, traceShow)
 import Game hiding (Event, Result)
 import qualified Game
 import SharedModel (SharedModel)
 import qualified SharedModel
 import Turn (Turn, turnToPlayerSpot)
 
+-- | Events that place creatures on the board. This function guarantees
+-- that the returned events are solely placements (no neutral cards), so
+-- that playing them with 'Game.playAll' returns an empty list of 'Game.Event'
 placeCards ::
   SharedModel ->
   Board Core ->
@@ -66,8 +70,8 @@ aiPlay shared board turn =
       map
         ( \events ->
             case Game.playAll shared board events of
-              Left msg -> trace ("Unexpected case: " ++ Text.unpack msg) Nothing
-              Right (Game.Result board' _) -> Just (boardScore board' turn, events)
+              Left msg -> trace ("Maybe unexpected? " ++ Text.unpack msg) Nothing
+              Right (Game.Result board' () _) -> Just (boardScore board' turn, events)
         )
         possibles
         & catMaybes
@@ -94,16 +98,22 @@ boardScore board turn =
         cSpotAndMayCreatures
 
 -- | Events for playing all cards of the hand, in order. Each card
--- is placed at an optimal position.
+-- is played optimally.
 aiPlayHand :: SharedModel -> Board Core -> Turn -> [Game.Event]
 aiPlayHand shared board turn =
   case aiPlayFirst shared board turn of
     Nothing -> []
-    Just place ->
-      case Game.play shared board place of
-        Left msg -> error $ Text.unpack msg -- We shouldn't generate invalid Place actions
-        Right (Game.Result b' _) ->
-          place : aiPlayHand shared b' turn
+    Just event ->
+      case Game.playAll shared board [event] of
+        Left msg ->
+          traceShow ("Cannot play first card of hand: " ++ Text.unpack msg) $
+            aiPlayHand shared board' turn
+          where
+            pSpot = turnToPlayerSpot turn
+            hand' = boardToHand board pSpot & tail
+            board' = boardSetHand board pSpot hand' -- Skip first card
+        Right (Game.Result b' () _) ->
+          event : aiPlayHand shared b' turn
 
 -- | Take the hand's first card (if any) and return a [Place] event
 -- for best placing this card.
@@ -129,7 +139,7 @@ aiPlayFirst shared board turn =
       ]
     liftFstMaybe (Nothing, _) = Nothing
     liftFstMaybe (Just i, a) = Just (i, a)
-    takeBoard (Game.Result b _) = b
+    takeBoard (Game.Result b _ _) = b
 
 placements ::
   Board Core ->
@@ -170,12 +180,16 @@ scorePlace board pSpot cSpot =
     maluses = [lineMalus, victoryPointsMalus]
 
 -- | The score of a card. Most powerful cards return a smaller value.
--- Negative values returned.
+-- Negative values returned. FIXME @smelc Return positive values
 scoreCard :: Card Core -> Int
 scoreCard = \case
   CreatureCard Creature {..} ->
     sum $ [- hp, - attack, - (fromMaybe 0 moral)] ++ map scoreSkill skills
-  NeutralCard _ -> error "NeutralCard unsupported"
+  NeutralCard NeutralObject {neutral} ->
+    case neutral of
+      Health -> -1
+      InfernalHaste -> -10
+      Life -> -3
   ItemCard _ -> error "ItemCard unsupported"
 
 -- | The score of a skill, smaller values are better. Negative values returned.

app/shared/Board.hs
@@ -411,9 +411,9 @@ spotToLens =
 
 appliesTo :: Neutral -> Board Core -> PlayerSpot -> CardSpot -> Bool
 appliesTo n board pSpot cSpot =
-  case boardToInPlaceCreature board pSpot cSpot of
-    Nothing -> False
-    Just creature -> Card.appliesTo n creature
+  case (Card.targetKind n, boardToInPlaceCreature board pSpot cSpot) of
+    (Card.CardTargetKind, Just _) -> True
+    _ -> False
 
 ------------------------------
 -- Now onto fancy rendering --

app/shared/Card.hs
@@ -246,10 +246,14 @@ teamDeck cards t =
         Human -> 1 ** Health ++ 1 ** Life
         Undead -> 2 ** InfernalHaste
 
--- | Whether a neutral card can be applied to the given creature
-appliesTo :: Neutral -> Creature Core -> Bool
-appliesTo n _ =
+-- FIXME @smelc Could I use type-level programming to use Game.Target's
+-- constructors as values instead of this?
+data TargetKind = CardTargetKind | PlayerTargetKind
+
+-- | The kind of Game target a neutral likes
+targetKind :: Neutral -> TargetKind
+targetKind n =
   case n of
-    Health -> True
-    Life -> True
-    InfernalHaste -> False
+    Health -> CardTargetKind
+    Life -> CardTargetKind
+    InfernalHaste -> PlayerTargetKind

app/shared/Game.hs
@@ -17,6 +17,7 @@ module Game
     nextAttackSpot,
     enemySpots,
     Event (..),
+    PolyResult (..),
     Result (..),
     play,
     playAll,
@@ -76,8 +77,14 @@ data Event
     Place' Target CreatureID
   deriving (Eq, Generic, Show)
 
-data Result = Result (Board Core) (Board UI)
+-- | The polymorphic version of 'Result'. Used for implementors that
+-- do not return events and hence instantiate 'a' by ()
+data PolyResult a = Result (Board Core) a (Board UI)
   deriving (Eq, Show)
+
+-- | The result of playing an 'Event': an updated board, the next event
+-- (if any), and the animations
+type Result = PolyResult (Maybe Event)
 
 reportEffect ::
   MonadWriter (Board UI) m =>
@@ -105,14 +112,29 @@ play shared board action =
     & build
   where
     build (Left err) = Left err
-    build (Right (b, a)) = Right $ Result b a
+    build (Right ((b, es), a)) = Right $ Result b es a
 
-playAll :: SharedModel -> Board Core -> [Event] -> Either Text Result
-playAll _ board [] = Right $ Result board mempty
-playAll shared board (e : events) = do
-  Result board' boardui' <- play shared board e
-  Result board'' boardui'' <- playAll shared board' events
-  return $ Result board'' (boardui' <> boardui'')
+-- | Please avoid calling this function if you can. Its effect is
+-- difficult to predict because of cards that create events, as these
+-- events are played right away (instead of being enqueued in the main loop
+-- like when playing Infernal Haste interactively). FIXME @smelc
+-- take an additional parameter telling whether to mimic the main loop
+-- (for Game.Attack events).
+playAll :: SharedModel -> Board Core -> [Event] -> Either Text (PolyResult ())
+playAll shared board es = go board mempty es
+  where
+    go b anims [] = Right $ Result b () anims
+    go b anims (e : tl) = do
+      Result board' new anims' <- play shared b e
+      -- /!\ We're enqueueing the event created by playing 'e' i.e. 'new',
+      -- before the rest of the events ('tl'). This means 'tl' will be played in a state
+      -- that is NOT the one planned when building 'e : tl' (except if the
+      -- caller is very smart). We have no way around that: this is caused
+      -- by Infernal Haste that creates events when playing it. But this means
+      -- we shouldn't be too hard with events that yield failures. Maybe
+      -- they are being played in an unexpected context (imagine if
+      -- Infernal Haste clears the opponent's board, next spells may be useless).
+      go board' (anims <> anims') $ maybeToList new ++ tl
 
 playM ::
   MonadError Text m =>
@@ -120,18 +142,21 @@ playM ::
   SharedModel ->
   Board Core ->
   Event ->
-  m (Board Core)
-playM _ board (Attack pSpot cSpot _ _) = Game.attack board pSpot cSpot
-playM _ board NoPlayEvent = return board
+  m (Board Core, Maybe Event)
+playM _ board (Attack pSpot cSpot _ _) = do
+  board' <- Game.attack board pSpot cSpot
+  return (board', Nothing)
+playM _ board NoPlayEvent = return (board, Nothing)
 playM shared board (Place target (handhi :: HandIndex)) = do
   ident <- lookupHand hand handi
   let card = unsafeIdentToCard shared ident & unliftCard
   case (target, card) of
-    (CardTarget pSpot cSpot, CreatureCard card) ->
-      playCardTargetM board' pSpot cSpot card
-    (PlayerTarget _, NeutralCard NeutralObject {neutral}) ->
-      playPlayerTargetM board' playingPlayer pSpot neutral
-    _ -> throwError $ Text.pack $ "Wrong (target, card): (" ++ show target ++ ", " ++ show card ++ ")"
+    (CardTarget pSpot cSpot, CreatureCard card) -> do
+      board'' <- playCardTargetM board' pSpot cSpot card
+      return (board'', Nothing)
+    (_, NeutralCard NeutralObject {neutral}) ->
+      playPlayerTargetM board' playingPlayer target neutral
+    _ -> throwError $ Text.pack $ "Wrong (Target, card) combination: (" ++ show target ++ ", " ++ show card ++ ")"
   where
     handi = unHandIndex handhi
     (hand, hand') = (boardToHand board pSpot, deleteAt handi hand)
@@ -159,11 +184,34 @@ playPlayerTargetM ::
   MonadWriter (Board UI) m =>
   Board Core ->
   PlayerSpot ->
-  PlayerSpot ->
+  Target ->
   Neutral ->
-  m (Board Core)
-playPlayerTargetM board playingPlayer pSpot n =
-  undefined
+  m (Board Core, Maybe Event)
+playPlayerTargetM board _playingPlayer target n =
+  case (n, target) of
+    (InfernalHaste, PlayerTarget pSpot) ->
+      return (board, event)
+      where
+        event =
+          nextAttackSpot board pSpot Nothing
+            <&> (\cSpot -> Attack pSpot cSpot True False)
+    (Health, CardTarget pSpot cSpot) -> do
+      board' <- addHitpoints pSpot cSpot 1
+      return (board', Nothing)
+    (Life, CardTarget pSpot cSpot) -> do
+      board' <- addHitpoints pSpot cSpot 3
+      return (board', Nothing)
+    _ -> throwError $ Text.pack $ "Wrong (Target, Neutral) combination: (" ++ show target ++ ", " ++ show n ++ ")"
+  where
+    addHitpoints pSpot cSpot hps =
+      case boardToInPlaceCreature board pSpot cSpot of
+        Nothing -> return board
+        Just c@Creature {hp} ->
+          -- FIXME @smelc Generate animation showing hit points increase
+          return board'
+          where
+            c' = c {hp = hp + hps}
+            board' = boardSetCreature board pSpot cSpot c'
 
 playCardTargetM ::
   MonadError Text m =>

app/test/Generators.hs
@@ -133,6 +133,7 @@ instance Arbitrary Game.Target where
   shrink = genericShrink
 
 instance Arbitrary Game.Event where
+  -- FIXME @smelc Generate Event that satisfy the Neutral<->Target invariant
   arbitrary = genericArbitraryU
   shrink = genericShrink
 

app/test/Test.hs
@@ -18,7 +18,7 @@ import Data.List as List
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import qualified Data.Set as Set
-import qualified Game (Event (..), Result (..), Target (..), attackOrder, playAll)
+import qualified Game (Event (..), PolyResult (..), Target (..), attackOrder, playAll)
 import Generators
 import Json
 import Movie
@@ -51,7 +51,7 @@ testAIRanged :: SharedModel -> Turn -> Board Core
 testAIRanged shared turn =
   case Game.playAll shared board events of
     Left _ -> error "AI failed"
-    Right (Game.Result board' _) -> board'
+    Right (Game.Result board' () _) -> board'
   where
     archer = IDC $ CreatureID Archer Undead
     board = boardAddToHand emptyBoard (turnToPlayerSpot turn) archer
@@ -183,7 +183,7 @@ testAIPlace shared =
                 Pretty (ignoreErrMsg (Game.playAll shared board events)) `shouldBe` Pretty (ignoreErrMsg (Game.playAll shared board events'))
   where
     ignoreErrMsg (Left _) = Nothing
-    ignoreErrMsg (Right (Game.Result board' _)) = Just board'
+    ignoreErrMsg (Right (Game.Result board' () _)) = Just board'
     spotsDiffer (Game.Place' (Game.CardTarget pSpot1 cSpot1) _) (Game.Place' (Game.CardTarget pSpot2 cSpot2) _) =
       pSpot1 /= pSpot2 || cSpot1 /= cSpot2
     spotsDiffer _ _ = error "Only Place' events should have been generated"

commit 1f419673fa597c033f6d9d228cef0f33715f5045

Fix inverted conditional that broke End Turn in 64eddd3cda46da21bacb913470151db942b981c2

app/shared/Update.hs
@@ -306,14 +306,14 @@ updateGameModel m@GameModel {board, gameShared} (GamePlay gameEvent) _ =
       where
         m' = m {board = board', anims = anims'}
         event = case (gameEvent, nexts) of
-          (Game.Attack pSpot cSpot continue end, Nothing) ->
+          (Game.Attack pSpot cSpot continue changeTurn, Nothing) ->
             -- enqueue resolving next attack if applicable
             case (continue, Game.nextAttackSpot board pSpot (Just cSpot)) of
               (False, _) -> terminator
               (True, Nothing) -> terminator
-              (True, Just cSpot') -> Just $ GamePlay $ Game.Attack pSpot cSpot' True end
+              (True, Just cSpot') -> Just $ GamePlay $ Game.Attack pSpot cSpot' True changeTurn
             where
-              terminator = if end then Nothing else Just GameIncrTurn
+              terminator = if changeTurn then Just GameIncrTurn else Nothing
           (Game.Attack {}, Just e) ->
             error $ "Cannot mix Game.Attack and events when enqueuing but got event: " ++ show e
           (_, Nothing) -> Nothing