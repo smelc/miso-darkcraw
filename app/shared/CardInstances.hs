3b21b355ac6f607ef1c88e9d2fc4de5e3958b995
smelc <smelc@users.noreply.github.com>
2021-01-01 17:02:40 +0100

    Add Startable class, preparing for resetting librarian's DrawCard' skill at start of turn

---
+++ app/shared/CardInstances.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module CardInstances where
+
+import Card
+
+-- | Class for things that can be reset at the start of a turn
+class Startable a where
+  start :: a -> a
+  start = id
+
+instance Startable SkillCore where
+  start (DrawCard' False) = DrawCard' True
+  start a = a
+
+instance Startable (Creature 'Core) where
+  start Creature {..} = Creature {skills = map start skills, ..}
