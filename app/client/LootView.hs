cda9dcdeb686b394a91f8d2a127ab345097d4bfd
smelc <smelc@users.noreply.github.com>
2021-08-24 18:55:01 +0200

    Minimal introduction of LootView. Do ./scripts/dl-large-assets.sh to download the new corresponding asset

--- app/client/LootView.hs
+++ app/client/LootView.hs
@@ -1,9 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module LootView where
 
+import qualified Constants
+import qualified Data.Map.Strict as Map
 import Miso
-import Model (LootModel)
+import Miso.String
+import Model (LootModel (..))
 import Update
 import ViewInternal
 
+-- |
+-- Module to display the view reached after a successful end of 'GameView'.
+-- This view sits between two games, where the player can pick new
+-- cards to augment its deck.
+-- |
 view :: LootModel -> Styled (View Action)
-view = undefined
+view LootModel {} = do
+  return $
+    div_
+      [style_ $ bgStyle 0]
+      []
+  where
+    bgStyle :: Int -> Map.Map MisoString MisoString
+    bgStyle z =
+      zpltwh z Relative 0 0 Constants.lobbiesPixelWidth Constants.boardPixelHeight
+        <> "background-image" =: Constants.assetsUrl "loot-forest-sand.png"
