1aebae1db7d81e47680e616efadf3d8aa65a681d
smelc <smelc@users.noreply.github.com>
2021-12-31 10:44:10 +0100

    Add new executable (updb) to update balance data

---
+++ app/updb/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- This main executes the balance, specifying to update the
+-- regression data; so that the next test of the balance is guaranteed to pass.
+--
+-- Build with @cabal build updb@, run with @cabal run updb@ (in a @nix-shell@)
+module Main where
+
+import qualified Balance
+import qualified SharedModel
+import Test.Hspec
+
+main :: IO ()
+main = do
+  _ <- hspec $ Balance.main (SharedModel.unsafeGet) True
+  return ()
