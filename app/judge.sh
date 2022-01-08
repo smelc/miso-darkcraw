9d7f7ac4c738b65a1007215fbf9fec66a575249c
smelc <smelc@users.noreply.github.com>
2022-01-08 14:09:03 +0100

    judge.sh: be more robust

--- app/judge.sh
+++ app/judge.sh
@@ -2,6 +2,8 @@
 
 EXEC="dist-newstyle/build/x86_64-linux/ghc-8.6.5/app-0.1.0.0/x/judge/build/judge/judge"
 
-[[ -e "$EXEC" ]] || { echo "$EXEC doesn't exist. Please run 'cabal build judge'."; exit 1; }
+nix-shell --run 'cabal build judge' || { echo "nix-shell --run 'cabal build judge' failed. Cannot proceed."; exit 1; }
+
+[[ -e "$EXEC" ]] | { echo "$EXEC should exist by now. Exiting."; exit 1; }
 
 ./$EXEC
