a828e4740ce25c3b2d2a30d88d05ff8f8a89ad92
smelc <smelc@users.noreply.github.com>
2022-01-09 18:02:21 +0100

    [judgebot] Automatic balance update

--- app/test/Weight.hs
+++ app/test/Weight.hs
@@ -23,11 +23,11 @@ balances =
     (Human, Campaign.Level0, Evil, 61, 3, 0),
     (Human, Campaign.Level0, Undead, 61, 3, 0),
     (Human, Campaign.Level0, ZKnights, 48, 15, 1),
-    (Evil, Campaign.Level0, Undead, 31, 32, 1),
+    (Evil, Campaign.Level0, Undead, 32, 30, 2),
     -- Level1
     (Human, Campaign.Level1, Evil, 182, 8, 2),
     (Human, Campaign.Level1, Undead, 354, 27, 3),
-    (Evil, Campaign.Level1, Undead, 53, 74, 1)
+    (Evil, Campaign.Level1, Undead, 50, 75, 3)
   ]
 
 -- | Returns the balance of the given matchup
